cd914f8846c88bac662ec7f074301a9a90ff3420
sundaravaradhanA <sundaravaradhanmadurai_bec27@mepcoeng.ac.in>
2026-02-24 16:38:20 +0530

    Add files via upload

---
+++ 2_forkreturn.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+	pid_t p;
+	p=fork();
+	printf("\nFork returns:%d",p);
+	printf("\nWelcome: My process id:%d, My parent's id:%d",getpid(),getppid());
+	return 0;
+}
+
+
